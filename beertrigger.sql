99cb794c3291dd6bd30c06256335bdd4cf493224
michael <michael@bodhibox.(none)>
2013-11-13 19:27:11 -0700

    updated trigger price diff

--- beertrigger.sql
+++ beertrigger.sql
@@ -5,15 +5,17 @@
 	price character varying
 );
 
+DROP FUNCTION process_beer_audit();
 CREATE OR REPLACE FUNCTION process_beer_audit() RETURNS TRIGGER AS $process_beer_audit$
+DECLARE oldprice character varying;
 BEGIN
 	-- This trigger checks to see if a row already exists, updates it if it does,
 	-- inserts it if it does not, and then makes a note of any price changes in an
 	-- audit table.
-	IF EXISTS (SELECT 1 FROM beers WHERE store = NEW.store AND link = NEW.link)
+	IF EXISTS (SELECT price INTO oldprice FROM beers WHERE store = NEW.store AND link = NEW.link)
 	THEN
-		IF (NEW.price != price) THEN 
-			INSERT INTO  price_audit VALUES(now(), NEW.store, NEW.link,NEW.price)
+		IF (NEW.price != oldprice) THEN 
+			INSERT INTO  price_audit VALUES(now(), NEW.store, NEW.link,NEW.price);
 		END IF;
 	ELSE
 		RETURN NEW;
@@ -22,6 +24,7 @@ END;
 $process_beer_audit$
 LANGUAGE plpgsql;
 
+DROP TRIGGER beer_audit ON beers;
 CREATE TRIGGER beer_audit
 BEFORE INSERT OR UPDATE ON beers
 	FOR EACH ROW EXECUTE PROCEDURE process_beer_audit();
