800f6b9a48197af5be5f137447dae5a7a6db151d
michael <michael@bodhibox.(none)>
2013-11-18 17:28:53 -0700

    Fixed trigger and trigger function to update beers table and audit table.

---
+++ beer_audit_trg.sql
@@ -0,0 +1,9 @@
+﻿-- Trigger: beer_audit on beers
+
+-- DROP TRIGGER beer_audit ON beers;
+
+CREATE TRIGGER beer_audit
+  BEFORE INSERT
+  ON beers
+  FOR EACH ROW
+  EXECUTE PROCEDURE process_beer_audit();
---
+++ process_beer_audit.sql
@@ -0,0 +1,29 @@
+﻿-- Function: process_beer_audit()
+
+-- DROP FUNCTION process_beer_audit();
+
+CREATE OR REPLACE FUNCTION process_beer_audit()
+  RETURNS trigger AS
+$BODY$
+DECLARE oldprice character varying;
+BEGIN
+	-- This trigger checks to see if a row already exists, updates it if it does,
+	-- inserts it if it does not, and then makes a note of any price changes in an
+	-- audit table.
+	IF EXISTS (SELECT 1 FROM beers WHERE store = NEW.store AND link = NEW.link)
+	THEN
+		SELECT price into oldprice from beers where store = NEW.store and link = NEW.link;
+		IF (NEW.price != oldprice) THEN 
+			INSERT INTO  price_audit VALUES(now(), NEW.store, NEW.link,NEW.price);
+			UPDATE beers SET price = NEW.price WHERE store = NEW.store AND link = NEW.link;
+		END IF;
+		RETURN NULL;
+	ELSE
+		RETURN NEW;
+	END IF;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION process_beer_audit()
+  OWNER TO michael;
